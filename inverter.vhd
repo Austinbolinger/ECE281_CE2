7eed6a6960874d7a8ecd14373756eb328d6cdf42
Austin Bolinger <c16austin.bolinger@usafa.edu>
2014-02-05 21:48:50 -0700

    first upload

---
+++ inverter.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:46:46 02/03/2014 
+-- Design Name: 
+-- Module Name:    inverter - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity inverter is
+    Port ( I : in  STD_LOGIC;
+           O : out  STD_LOGIC);
+end inverter;
+
+architecture Behavioral of inverter is
+
+signal I_NOT : STD_LOGIC;
+	
+begin
+
+I_NOT <= not I;
+O <= I_NOT;
+
+end Behavioral;
+
---
+++ myand3.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:04:38 02/03/2014 
+-- Design Name: 
+-- Module Name:    myand3 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity myand3 is
+    Port ( I0 : in  STD_LOGIC;
+           I1 : in  STD_LOGIC;
+           I2 : in  STD_LOGIC;
+           O : out  STD_LOGIC);
+end myand3;
+
+architecture Behavioral of myand3 is
+
+signal I0_NOT, I1_NOT : STD_LOGIC;
+
+begin
+	
+	I0_NOT <= not I0;
+	I1_NOT <= not I1;
+	O <= I0 and I1 and I2;
+
+end Behavioral;
+
